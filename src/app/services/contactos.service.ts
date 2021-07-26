191a0fa763120b645a84075ec08e4086395e924f
Jesus Marquez <jesmqz@gmail.com>
2021-07-26 18:11:56 -0400

    Set environment variable

--- src/app/services/contactos.service.ts
+++ src/app/services/contactos.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ContactoModel } from '../models/contacto.model';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactosService {
-  private url = 'http://localhost:3000';
+  private apiEndPoint: string = "";
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.apiEndPoint = environment.apiEndPoint;
+  }
 
   crearContacto( contacto: ContactoModel) {
 
-    return this.http.post(`${ this.url }/contacto`, contacto)
+    return this.http.post(`${ this.apiEndPoint }/contacto`, contacto)
             .pipe(
               map( (resp: any ) => {
                 return contacto;
